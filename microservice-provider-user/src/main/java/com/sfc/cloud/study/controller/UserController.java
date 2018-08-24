bb949ca22d719ea80f1ed085349805f858b2eb81
Prophet Xu <slowfishcolor@gmail.com>
2018-08-24 14:22:19 +0800

    Add Eureka for simple service discovery

---
+++ microservice-provider-user/src/main/java/com/sfc/cloud/study/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.sfc.cloud.study.controller;
+
+import com.sfc.cloud.study.entity.User;
+import com.sfc.cloud.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.userRepository.findOne(id);
+  }
+}
---
+++ microservice-provider-user/src/main/java/com/sfc/cloud/study/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sfc.cloud.study.repository;
+
+import com.sfc.cloud.study.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
